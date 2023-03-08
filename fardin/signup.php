42d07933a184f2761c4fc5144abda2295b830488
unknown <mamuin@buffalo.edu>
2023-03-08 11:57:57 -0500

    Sign up page backend

--- fardin/signup.php
+++ fardin/signup.php
@@ -1,46 +1,45 @@
 <?php
-// Change this to your connection info.
-$DATABASE_HOST = "localhost";
-$DATABASE_USER = "user";
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'user_account';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Get the user input
+    $username = $_POST['uid'];
+    $email = $_POST['email'];
+    $password = $_POST['pwd'];
+
+    // Validate the user input (e.g. check for empty fields, validate email address, etc.)
+    // ...
+
+    // Hash the password for security
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Connect to the database (replace with your own database credentials)
+    $servername = "oceanus.cse.buffalo.edu:3306";
+    $username = "mamuin";
+    $password = "50424784";
+    $dbname = "mamuin_db";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Insert the new user data into the database
+    $sql = "INSERT INTO users (username, email, password)
+            VALUES ('$username', '$email', '$hashedPassword')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Redirect the user to the login page
+        header("Location: login.html");
+        exit;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+
 }
 
-//Checking is the account with that email exists.
-// We need to check if the account with that username exists.
-if ($stmt = $con->prepare('SELECT id, password FROM user WHERE email = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
-	$stmt->bind_param('s', $_POST['email']);
-	$stmt->execute();
-	$stmt->store_result();
-	// Store the result so we can check if the account exists in the database.
-	if ($stmt->num_rows > 0) {
-		// Username already exists
-		echo 'The email exists, please choose another!';
-	} 
-    
-    else {
-		// Email doesn't exists, insert new account
-if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
-	// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
-	$stmt->execute();
-	echo 'You have successfully registered! You can now login!';
-} else {
-	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
-	echo 'Could not prepare statement!';
-}
-	}
-	$stmt->close();
-} else {
-	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
-	echo 'Could not prepare statement!';
-}
-$con->close();
 ?>
