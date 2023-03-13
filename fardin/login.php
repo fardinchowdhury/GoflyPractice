5608ba5ec6747856cf2269667a0dab17f343570c
Md Hyder <mdhyder@buffalo.edu>
2023-03-12 23:22:46 -0400

    Nneed to fix login.php

--- fardin/login.php
+++ fardin/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once("config.php");
+require_once('config.php');
 // Establish database connection
 session_start();
 
@@ -9,29 +9,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
     $username = mysqli_real_escape_string($db_connection, $_POST['uid']);
     $password = mysqli_real_escape_string($db_connection, $_POST['pwd']);
-
-   //hashing the input password
-   $hashP = password_hash($password, PASSWORD_DEFAULT);
-
-   if(password_verify($password, ))
     
 
     // Query the database
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$hashP'";
+    $sql = "SELECT * FROM users WHERE username='$username'";
     $result = mysqli_query($db_connection, $sql);
 
     // Check if the query returned any rows
     if (mysqli_num_rows($result) == 1) {
-        // Login successful
-        session_start();
-        $_SESSION['username'] = $username;
-        header("Location: landing.php");
-        exit;
+        // Get the user's data
+        $user = mysqli_fetch_assoc($result);
+        
+        // Verify the password
+        if (password_verify($password, $user['password'])) {
+            // Login successful
+            session_start();
+            $_SESSION['username'] = $username;
+            header("Location: landing.php");
+            exit;
+        } else {
+            // Login failed
+            echo "Invalid username or password.";
+        }
     } else {
         // Login failed
         echo "Invalid username or password.";
     }
 }
 
-$db_connection->close();
+mysqli_close($db_connection);
 ?>
