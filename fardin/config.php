bda20a8cfb677dbe1f18901a52528b34b38fd5cf
MDHyder-eng <63564635+MDHyder-eng@users.noreply.github.com>
2023-03-18 21:04:40 -0400

    ch

--- fardin/config.php
+++ fardin/config.php
@@ -3,15 +3,15 @@ global $db_connection;
 
 //Connect to the databse using mysqli
 $servername = "oceanus.cse.buffalo.edu:3306";
-$username = "mamuin";
+$username = "mdhyder";
 $password = "50424784";
 $dbname = "cse442_2023_spring_team_y_db";
 
 $db_connection = new mysqli($servername, $username, $password, $dbname);
 
 //Check conncection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($db_connection->connect_error) {
+    die("Connection failed: " . $db_connection->connect_error);
 }
 
 ?>
